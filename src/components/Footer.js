ced482261a84eb6a0ecd36d385ce278866a5d8a6
david <noemail>
2021-07-05 18:12:50 +0200

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
 function Footer() {
     return (
-      <div className="bg-gradient-to-r from-blue-500 via-blue-400 to-blue-300 text-white">
+      <div className="bg-gradient-to-r from-blue-500 via-blue-400 to-blue-300 text-white text-center text-xl font-semibold">
         <nav>
-            <h2 className="text-2xl mx-3 my-6 inline-block">I am a footer</h2>
-            <a href='https://www.freepik.com/free-photos-vectors/background'>Background vector created by starline - www.freepik.com</a>
+            <div className="pt-6">
+                <a className="text-4xl mx-4"><i class="far fa-envelope"></i></a>
+                <a className="text-4xl mx-4"><i class="fab fa-linkedin"></i></a>
+                <button className="text-4xl mx-4"> <i class="fab fa-github-square"></i> </button>
+                     
+            </div>
+            <div className="py-6 italic">Site created by David Lidebrandt</div>
+            <div className="py-3">
+                <a href='https://www.freepik.com/free-photos-vectors/background'>Background vector created by starline - www.freepik.com</a>
+            </div>
         </nav>
       </div>
     );
